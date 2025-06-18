4a6ab354bf746cb53bd419610717f17f4e53fafb
girish-kanjiyani7 <girish.kanjiyani5040@gmail.com>
2025-06-18 13:43:50 -0400

    df

--- movie-react/src/App.jsx
+++ movie-react/src/App.jsx
@@ -1,36 +1,3 @@
-import { useEffect, useState } from 'react'
+
 import './App.css'
-
-const Card = ({title}) => {
-  const [hasLiked, sethasLiked] = useState(false);
-  const [count, setcount] = useState(0);
-
-  useEffect(()=> {
-    console.log(`${title} has been liked ${hasLiked}`);
-  }, [hasLiked]);
-
-  return (
-    
-    <div className='card' onClick={()=> setcount((prev)=> prev+1)}>
-    <h2> {title}<br /> {count}</h2>
-    <button onClick= {() => sethasLiked((prev)=> !prev)}>
-    {hasLiked ? "Liked": "Like"}
-
-    </button>
-    </div>
-  )
-}
-const App = () => {
-  
-  return (
-    <div className="card-container">
-    
-    
-    <Card title = "Star wars" />
-    <Card title = "The Lion king" />
-    <Card title = "Dasani"/>
-    </div>
-  )
-}
-
-export default App
+import
